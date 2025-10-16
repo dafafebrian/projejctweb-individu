e73b6818e5bcd8793532743d0d1a52ffeb367789
dafafebrian <dafafebrian830@gmail.com>
2025-10-16 20:09:58 +0700

    UPDATE DATABASES

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Berita;
 
 Route::get('/', function () {
     return view('welcome', [
@@ -18,9 +21,8 @@ Route::get('/profile', function () {
     return view('profile', [
         "title" => "Profile",
         "nama" => "Dafa Febrianto",
-        "nohp" => "08213697881",
-        "NIM" => "13242420033",
-        "foto" => "img/jokowi.jpg"
+        "nohp" => "08844444444",
+        "foto" => "img/jokowi.png"
     ]);
 });
 
@@ -36,66 +38,7 @@ Route::get('/contact', function () {
     ]);
 });
 
-Route::get('/berita', function () {
-    $data_berita = [
-        [
-            "judul" => "Berita satu",
-            "slug" => "berita-satu",
-            "penulis" => "Nadhif Basmalah",
-            "konten" => "Bergema sampai selamanya"
-        ],
-        [
-            "judul" => "Berita dua",
-            "slug" => "berita-dua",
-            "penulis" => ".Feast",
-            "konten" => "Tarot"
-        ],
-        [
-            "judul" => "Berita tiga",
-            "slug" => "berita-tiga",
-            "penulis" => "Season",
-            "konten" => "Wave to Earth"
-        ]
-    ];
+Route::get('/berita', [BeritaController::class, 'index']);
+Route::get('/berita/{slug}', [BeritaController::class, 'tampildata']);
 
-    return view('berita', [
-        "title" => "Berita",
-        "beritas" => $data_berita,
-    ]);
-});
-
-Route::get('/berita/{slug}', function ($slug) {
-    $data_berita = [
-        [
-            "judul" => "Berita satu",
-            "slug" => "berita-satu",
-            "penulis" => "Nadhif Basmalah",
-            "konten" => "Bergema sampai selamanya"
-        ],
-        [
-            "judul" => "Berita dua",
-            "slug" => "berita-dua",
-            "penulis" => ".Feast",
-            "konten" => "Tarot"
-        ],
-        [
-            "judul" => "Berita tiga",
-            "slug" => "berita-tiga",
-            "penulis" => "Season",
-            "konten" => "Wave to Earth"
-        ]
-    ];
-
-    $new_berita = [];
-    foreach ($data_berita as $berita) {
-        if ($berita["slug"] === $slug) {
-            $new_berita = $berita;
-            break;
-        }
-    }
-
-    return view('singleberita', [
-        "title" => "Berita",
-        "new_berita" => $new_berita,
-    ]);
-});
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
